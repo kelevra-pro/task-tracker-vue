2b111525922fd901d525f4add8c76d27d9b108d5
kelevra-pro <damtat8@gmail.com>
2022-07-11 16:35:36 +0300

    task component & v-for loop

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <h3>{{ task.text }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
